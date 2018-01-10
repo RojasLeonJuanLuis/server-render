0d269af5f906c441537aead787a62705aefe6854
RojasLeonJuanLuis <rojasleon122@gmail.com>
2018-01-09 18:17:37 -0600

    babel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const React = require('react')
-const renderToString = require('react-dom/server').renderToString
-const Home = require('./client/components/home').default
-const app = express()
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import Home from './client/components/home'
 
 app.get('/', (req, res) => {
   const content = renderToString(<Home />)
-  
+
   res.send(content)
 })
 
