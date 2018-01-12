7dfb5f9864084ecd6e0bbe11556651d8f302220a
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-01-12 16:15:15 -0600

    users list component

--- src/client/client.js
+++ src/client/client.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import Routes from './routes'
+import reducers from './reducers'
 
 const store = createStore(reducers, {}, applyMiddleware(thunk))
 
