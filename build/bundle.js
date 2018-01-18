f533fc9d1aff3c2a4c5fde752e429dda6ffa18b0
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-01-17 21:49:49 -0600

    dumping state to templates

--- build/bundle.js
+++ build/bundle.js
@@ -283,7 +283,7 @@ exports.default = function (req, store) {
     )
   ));
 
-  return '<html>\n      <head></head>\n      <body>\n        <div id="root">' + content + '</div>\n        <script src="bundle.js"></script>\n      </body>\n    </html>';
+  return '<html>\n      <head></head>\n      <body>\n        <div id="root">' + content + '</div>\n        <script>\n          window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + '\n        </script>\n        <script src="bundle.js"></script>\n      </body>\n    </html>';
 };
 
 /***/ }),
