17dc8e25f691a404df2b06f5e7e41f539dcd2b9f
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-01-17 22:16:12 -0600

    Proxy setup

--- build/bundle.js
+++ build/bundle.js
@@ -192,6 +192,10 @@ var _express2 = _interopRequireDefault(_express);
 
 var _reactRouterConfig = __webpack_require__(1);
 
+var _expressHttpProxy = __webpack_require__(21);
+
+var _expressHttpProxy2 = _interopRequireDefault(_expressHttpProxy);
+
 var _renderer = __webpack_require__(9);
 
 var _renderer2 = _interopRequireDefault(_renderer);
@@ -208,6 +212,12 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
+app.use('/api', (0, _expressHttpProxy2.default)('http://react-ssr-api.herokuapp.com', {
+  proxyReqOptDecorator: function proxyReqOptDecorator(opts) {
+    opts.header['x-forwarded-host'] = 'localhost:3000';
+    return opts;
+  }
+}));
 app.use(_express2.default.static('public'));
 
 app.get('*', function (req, res) {
@@ -523,6 +533,13 @@ exports.default = function () {
 /***/ (function(module, exports) {
 
 module.exports = require("serialize-javascript");
+
+/***/ }),
+/* 20 */,
+/* 21 */
+/***/ (function(module, exports) {
+
+module.exports = require("express-http-proxy");
 
 /***/ })
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import 'babel-polyfill'
 import express from 'express'
 import { matchRoutes } from 'react-router-config'
+import proxy from 'express-http-proxy'
 import renderer from './helpers/renderer'
 import createStore from './helpers/create-store'
 import Routes from './client/routes'
 
 const app = express()
 
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+  proxyReqOptDecorator(opts) {
+    opts.header['x-forwarded-host'] = 'localhost:3000'
+    return opts
+  }
+}))
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
@@ -18,7 +25,7 @@ app.get('*', (req, res) => {
   Promise.all(promises).then(() => {
     res.send(renderer(req, store))
   })
-  
+
 })
 
 app.listen(3000, () => {
